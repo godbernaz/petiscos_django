e9863da76524777da6dc3dbc2278370f833311a8
godbernaz <bernazbarros2000@gmail.com>
2024-11-20 19:55:01 +0000

    Added a new login and register form and pages, plus the logout function and some logic

---
+++ petiscos-web/webecom/mystore/forms.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+class SignUpForm(UserCreationForm):
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+	last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
+
+	class Meta:
+		model = User
+		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+	def __init__(self, *args, **kwargs):
+		super(SignUpForm, self).__init__(*args, **kwargs)
+
+		self.fields['username'].widget.attrs['class'] = 'form-control'
+		self.fields['username'].widget.attrs['placeholder'] = 'Nome do Utilizador'
+		self.fields['username'].label = ''
+		self.fields['username'].help_text = '<span class="form-text text-muted"><small>É necessário um nome de utilizador entre 10 a 20 letras e apenas é permitido os seguintes carateres: @/./+/-/_ </small></span>'
+
+		self.fields['password1'].widget.attrs['class'] = 'form-control'
+		self.fields['password1'].widget.attrs['placeholder'] = 'Palavra-chave'
+		self.fields['password1'].label = ''
+		self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>A tua palavra-passe não pode ser demasiado semelhante às tuas outras informações pessoais.</li><li> A tua palavra-passe deve conter pelo menos 8 caracteres.</li><li>A tua palavra-passe não pode ser uma palavra-passe habitualmente utilizada.</li><li>A tua palavra-passe não pode ser inteiramente numérica.</li></ul>'
+
+		self.fields['password2'].widget.attrs['class'] = 'form-control'
+		self.fields['password2'].widget.attrs['placeholder'] = 'Confirmar a tua palavra-chave.'
+		self.fields['password2'].label = ''
+		self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Introduza a mesma palavra-passe novamente, para verificação.</small></span>'
--- petiscos-web/webecom/mystore/views.py
+++ petiscos-web/webecom/mystore/views.py
@@ -1,6 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Products
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
+
 
 def home(request):
     products = Products.objects.all()
     return render(request, 'home.html', {'products':products}) 
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('Bem vindo, você entrou na sua conta!.'))
+            return redirect('home')
+        else: 
+            messages.success(request, ('Não conseguiste entrar, talvez tenha havido um problema na escrita?'))
+            return redirect('login')
+            
+    else:
+        return render(request, 'login.html', {}) 
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('Saiste da tua conta!, és sempre bem-vindo novamente!.'))
+    return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ('Conta criada com sucesso, podes agora entrar!.'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Alguma coisa aconteceu, mas tens de tentar novamente!'))
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form':form})
